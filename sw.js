ea1bb1f553b0887b744d8851703a574cc6709641
luiss-a11y <luis.s@jcacontadores.com.br>
2026-06-27 08:28:01 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION="impostor-v2";
+const CACHE_VERSION="impostor-v3";
 const CORE=["./","./index.html","./manifest.json"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE_VERSION).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()).catch(()=>self.skipWaiting()));});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE_VERSION).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
